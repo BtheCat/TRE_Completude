ad501dd38ef05e9b0e7d04e895cf5639247d0e38
Mahell <benjamincatinaud@gmail.com>
2021-03-07 10:58:34 +0100

    Add sem fixpoint

--- tre_completeness_src.v
+++ tre_completeness_src.v
@@ -2,3 +2,10 @@ Inductive form :=
   | Atome : nat -> form
   | And : form -> form -> form
   | Or : form -> form -> form.
+
+Fixpoint sem (M : nat -> Prop) (A : form) :=
+  match A with
+    | Atome n => M n
+    | And A1 A2 => (sem M A1) /\ (sem M A2)
+    | Or A1 A2 => (sem M A1) \/ (sem M A2)
+  end.

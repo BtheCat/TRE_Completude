bb4d842fd758334eb958ee8b4e4c3da2d5025c88
Mahell <benjamincatinaud@gmail.com>
2021-03-16 10:53:00 +0100

    Add comments to prepare forcing step

--- tre_completeness_src.v
+++ tre_completeness_src.v
@@ -1,3 +1,5 @@
+(* Require Import Forcing *)
+
 Inductive form :=
   | Atome : nat -> form
   | And : form -> form -> form
@@ -17,6 +19,7 @@ Inductive included : context -> context -> Prop :=
   | included_cons_no : forall A ctx ctx', included ctx ctx' -> included ctx (cons A ctx')
   | included_cons_yes : forall A ctx ctx', included ctx ctx' -> included (cons A ctx) (cons A ctx').
 
-
 (*Definition valid M A := forall (M : nat -> Prop) (A : form), sem M A.*)
 Definition valid A := forall (M : nat -> Prop), sem M A.
+
+(* Forcing Definition completeness : valid A -> provable nil A using context included *)

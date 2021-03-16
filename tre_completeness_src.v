ac7dcc3ee011ca500b783747e0c48ee9084a5f15
Mahell <benjamincatinaud@gmail.com>
2021-03-16 10:50:35 +0100

    Add included and valid definition

--- tre_completeness_src.v
+++ tre_completeness_src.v
@@ -5,9 +5,18 @@ Inductive form :=
 
 Fixpoint sem (M : nat -> Prop) (A : form) :=
   match A with
-    | Atome n => M n
+    | Atome n   => M n
     | And A1 A2 => (sem M A1) /\ (sem M A2)
-    | Or A1 A2 => (sem M A1) \/ (sem M A2)
+    | Or A1 A2  => (sem M A1) \/ (sem M A2)
   end.
 
-Definition ctx := list form.
+Definition context := list form.
+
+Inductive included : context -> context -> Prop :=
+  | included_nil : included nil nil
+  | included_cons_no : forall A ctx ctx', included ctx ctx' -> included ctx (cons A ctx')
+  | included_cons_yes : forall A ctx ctx', included ctx ctx' -> included (cons A ctx) (cons A ctx').
+
+
+(*Definition valid M A := forall (M : nat -> Prop) (A : form), sem M A.*)
+Definition valid A := forall (M : nat -> Prop), sem M A.
